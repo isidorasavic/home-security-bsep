f2ae194cdee16d2550dc55edeb0ba8c82451aa1b
isidorasavic <isidora99savic@gmail.com>
2022-04-10 17:22:38 +0200

    Initial project

---
+++ home-security/home-security/src/main/java/com/homesecurity/homesecurity/HomeSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.homesecurity.homesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeSecurityApplication.class, args);
+	}
+
+}
---
+++ home-security/home-security/src/test/java/com/homesecurity/homesecurity/HomeSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homesecurity.homesecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
