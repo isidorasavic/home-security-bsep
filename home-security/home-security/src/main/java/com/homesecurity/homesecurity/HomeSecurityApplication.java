d13bc4af4edd7600e6232944040c1e91458e3828
NenadPetkovicSW <nenad.petkovic@uns.ac.rs>
2022-05-19 01:28:34 +0200

    Backend se pokrece

--- home-security/home-security/src/main/java/com/homesecurity/homesecurity/HomeSecurityApplication.java
+++ home-security/home-security/src/main/java/com/homesecurity/homesecurity/HomeSecurityApplication.java
@@ -2,13 +2,13 @@ package com.homesecurity.homesecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class HomeSecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeSecurityApplication.class, args);
-		System.out.println( "Hello World!" );
 	}
 
 }
