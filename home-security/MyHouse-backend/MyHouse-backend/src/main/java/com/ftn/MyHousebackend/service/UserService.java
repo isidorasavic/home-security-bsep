de2da97e917cc8a4517546ab691af81cff8e5922
NenadPetkovicSW <nenad.petkovic@uns.ac.rs>
2022-06-29 19:09:06 +0200

    Token

--- home-security/MyHouse-backend/MyHouse-backend/src/main/java/com/ftn/MyHousebackend/service/UserService.java
+++ home-security/MyHouse-backend/MyHouse-backend/src/main/java/com/ftn/MyHousebackend/service/UserService.java
@@ -18,6 +18,7 @@ import com.ftn.MyHousebackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
--- home-security/admin-front/src/app/login/login.component.ts
+++ home-security/admin-front/src/app/login/login.component.ts
@@ -23,10 +23,14 @@ export class LoginComponent implements OnInit {
       this.roles = this.tokenStorage.getUser().roles;
     }
   }
+
   onSubmit(): void {
     const { username, password } = this.form;
     this.authService.login(username, password).subscribe({
       next: data => {
+
+        //let pom = JSON.parse(atob(data.accessToken.split('.')[1]));
+
         this.tokenStorage.saveToken(data.accessToken);
         this.tokenStorage.saveUser(data.username);
         this.tokenStorage.saveUserRole(data.role);
